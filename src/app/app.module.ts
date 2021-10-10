138fea406e9af58dc78719803c849635e197fa86
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-10-10 21:16:53 +0300

    This commit creates a directive to highlight the quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteInfoComponent } from './quote-info/quote-info.component';
+import { DayCountPipe } from './day-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { HighlightVoteDirective } from './highlight-vote.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
+    QuoteInfoComponent,
+    DayCountPipe,
     QuoteFormComponent,
-    QuoteInfoComponent
+    HighlightVoteDirective
   ],
   imports: [
     BrowserModule,
